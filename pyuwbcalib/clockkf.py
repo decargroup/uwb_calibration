6576c1af3b321458ff2547bd5301b772bf3cfb56
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2022-04-21 23:18:05 -0400

    Formatting.

--- pyuwbcalib/clockkf.py
+++ pyuwbcalib/clockkf.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 class ClockKF(object):
     """
-    Object to handle the Kalman Filter for the relative clock states between two DECAR/MRASL 
+    Object to handle the Kalman Filter for the relative clock states between two DECAR/MRASL
     UWB modules.
 
     PARAMETERS:
@@ -21,11 +22,11 @@ class ClockKF(object):
         Measurement variance.
     """
 
-    _c = 299702547 # speed of light
+    _c = 299702547  # speed of light
 
-    def __init__(self, meas, P0_delta=1000000, P0_gamma=1000000,
-                 Q_delta=100, Q_gamma=100, R=100):
+    def __init__(
+        self, meas, P0_delta=1000000, P0_gamma=1000000, Q_delta=100, Q_gamma=100, R=100
+    ):
         """
         Constructor
         """
-        
