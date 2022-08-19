421f2f8dfa8e9df405edda74038fb152524f227e
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2022-08-19 11:10:09 -0400

    Perform an fft on the optimal delay data

--- scripts/optimal_delay.py
+++ scripts/optimal_delay.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from csaps import csaps
 from scipy.interpolate import BSpline
 import pandas as pd
+from scipy import fft
 matplotlib.use('Qt5Agg')
 
 sns.set_theme()
@@ -24,7 +25,7 @@ for filename in os.listdir(folder_path):
     f = os.path.join(folder_path, filename)
 
     split_filename = filename.split('_')
-    delay = filename.split('_')[0]
+    delay = filename.split('_')[1]
     T = filename.split('_')[2][:-4] # in nanoseconds
     data = np.genfromtxt(f)
 
@@ -39,9 +40,9 @@ df = df.sort_values('delay')
 _c = 299702547
 
 def find_std(x):
-    upper_outliers = x>2.6
+    upper_outliers = x>3
     x_new = x[~upper_outliers]
-    lower_outliers = x_new<1.6
+    lower_outliers = x_new<1.4
     return np.std(x_new[~lower_outliers])/_c*1e9
 
 def get_Rt(exp_std,freq):
@@ -66,13 +67,42 @@ x = np.linspace(0,0.0065,1000)
 y = (T+x)*R + del_t2*T*R/x + del_t2*R + del_t2**2*T*R/x**2 + del_t2**2*R/x
 
 # %% 
-# Fit spline to Rt 
+## Plot FFT of std error 
+
+# New dataframe 
+df_fft = df[['delay','exp_std']].copy()
+
+# Remove outliers
+df_fft.drop(df_fft[df_fft.delay > 6500].index, inplace=True)
+
+# Average out stds of measurements with same delay
+df_fft = df_fft.groupby('delay', as_index=False)['exp_std'].mean()
+
+# Interpolate 
+df_fft.set_index(df_fft['delay'], drop=True, inplace=True)
+df_fft = df_fft.reindex(np.linspace(410,6499,(6499-410)+1))
+df_fft.interpolate(method='linear', inplace=True)
+
+delay = np.array(df_fft['delay']/1e6, dtype=float).copy()
+exp_std = np.array(df_fft['exp_std'])
+
+diff = np.array(exp_std-np.sqrt(R + del_t2/delay*R + (del_t2/delay)**2*R))
+n = diff.shape[0]
+data_fft = np.abs(fft.rfft(diff) / n)
+f = fft.rfftfreq(n,d=1e-6)
+plt.semilogx(f, data_fft)
+plt.xlabel(r'Frequency [Hz]')
+plt.title(r'FFT on standard deviation error vs. Second-response delay')
+# plt.scatter(delay,diff)
+plt.show(block=True)
+# %% 
+## Fit spline to Rt 
 
 # def moving_average(x, w):
 #     return np.convolve(x, np.ones(w), 'valid') / w
 
 # # Compute moving average of Rt
-delay = np.array(df['delay']/1e6, dtype=float)
+delay = np.array(df['delay']/1e6, dtype=float).copy()
 # delay_new = delay[4:-5]
 # Rt_new = moving_average(df['Rt'], 10)
 
@@ -112,7 +142,8 @@ axs[0].scatter(delay, df['Rt'], label=r'Experimental', s=1)
 # axs[0].plot(delay, Rt_smoothed, label=r'Experimental')
 axs[0].plot(x,y, 'r', label=r'Theoretical')
 axs[0].legend()
-axs[0].set_ylim(0,0.002)
+axs[0].set_ylim(0,0.0025)
+# axs[0].set_ylim(0,0.00025)
 axs[0].set_ylabel(r'$R_t$ [ns$^2$]')
 
 # Subplot 2: std
@@ -120,6 +151,7 @@ axs[1].scatter(delay, df['exp_std'], label=r'Experimental', s=1)
 axs[1].set_ylabel(r'Standard Deviation [ns]')
 axs[1].plot(x, np.sqrt(R + del_t2/x*R + (del_t2/x)**2*R),
             label=r'Theoretical', color='r')
+axs[1].set_ylim(0,0.5)
 
 # Subplot 3: freq
 axs[2].scatter(delay, df['freq'], label=r'Experimental', s=1)
