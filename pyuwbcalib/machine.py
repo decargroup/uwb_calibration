6f126be3326a7702ee06d9a5709d9f97e5fc203c
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2022-09-21 16:41:29 -0400

    Fully document machine.py.

--- pyuwbcalib/machine.py
+++ pyuwbcalib/machine.py
@@ -4,162 +4,280 @@ import numpy as np
 import pandas as pd
 
 class Machine(object):
-    """_summary_
+    """A base class for UWB machines.
+
+    This class reads configuration files and provides generic methods common to all 
+    machines.
 
     Attributes
     ----------
-    object : _type_
-        _description_
-
-    Examples
-    --------
+    max_ts_value : float
+        The maximum timestamp value that can be recorded by the machine before wrapping.
+        For example, if the timestamp is recorded as uint32, then max_ts_value = 2**32.
+    ts_to_ns: float
+        The conversion from timestamp value to nanoseconds.
+    ds_twr: bool
+        If double-sided TWR is used; False represents single-sided TWR.
+    fpp_exists: bool
+        If the first-path power is recorded.
+    rxp_exists: bool
+        If the average received power is recorded.
+    std_exists: bool
+        If the leading-edge-detection's uncertainty metric is recorded.
+    pose_path: str
+        The path of the file containing the ground-truth data.
+    uwb_path: str
+        The path of the file containing the UWB data.
+    machine_id: str
+        The ID of the machine.
+    tag_ids: list
+        The ID of the tags installed on this machine.
+    moment_arms: dict
+        keys: int
+            The ID of the tag
+        values: list
+            The 3D position of the tag relative to the machine's reference point,
+            in the machine's body frame.
+    pose_topic: str,
+        ROS ONLY. The topic name for the ground truth pose. 
+    uwb_topic: str
+        ROS ONLY. The topic name for the UWB topic.
+    uwb_fields: list
+        Fieldnames representing the fields collected through UWB.
     """
     def __init__(
         self,
         configs,
         id,
+        is_ros = True,
     ) -> None:
         """_summary_
 
         Parameters
         ----------
-        configs : _type_
-            _description_
-        id : _type_
-            _description_
+        configs : ConfigParser
+            A ConfigParser object where the config file is parsed.
+        id : int
+            The ID of the machine, as referenced in the config file.
+        is_ros : bool, optional
+            If this class is instantiated by RosMachine, by default True
         """
         
+        # Retrieve timestamping-specific params
         self.max_ts_value = eval(configs['PARAMS']['max_ts_value'])
         self.ts_to_ns = eval(configs['PARAMS']['ts_to_ns'])
         
+        # Retrieve booleans specifying what kind of data to expect
         self.ds_twr = eval(configs['PARAMS']['ds_twr'])
         self.fpp_exists = eval(configs['PARAMS']['fpp_exists'])
         self.rxp_exists = eval(configs['PARAMS']['rxp_exists'])
         self.std_exists = eval(configs['PARAMS']['std_exists'])
         
+        # Retrieve path information for ground-truth data 
         pose_dir = configs['POSE_PATH']['directory']
         self.pose_path = pose_dir + configs['POSE_PATH'][str(id)]
 
+        # Retrieve path information for UWB data
         uwb_dir = configs['UWB_PATH']['directory']
         self.uwb_path = uwb_dir + configs['UWB_PATH'][str(id)]
         
+        # Retrieve IDs and physical information about the machine
         self.machine_id = configs['MACHINES'][str(id)]
         self.tag_ids = eval(configs['TAGS'][str(id)])
         self.moment_arms = {tag:eval(configs['MOMENT_ARMS'][str(tag)]) for tag in self.tag_ids}
 
-        self.pose_topic = configs['POSE_TOPIC'][str(id)]
-
-        self.uwb_topic = configs['UWB_TOPIC'][str(id)]
+        # Retrieve ROS topic names for the data to be collected
+        if is_ros:
+            self.pose_topic = configs['POSE_TOPIC'][str(id)]
+            self.uwb_topic = configs['UWB_TOPIC'][str(id)]
+        
+        # Retrieve the message fields for the UWB data
         self.uwb_fields = [configs['UWB_MESSAGE'][key] for key in configs['UWB_MESSAGE'].keys()]
         
-    def convert_uwb_timestamps(self, ts_to_ns) -> None:
-        """_summary_
-
-        Parameters
-        ----------
-        ts_to_ns : _type_
-            _description_
+    def convert_uwb_timestamps(self) -> None:
+        """Covert UWB timestamps to nanoseconds.
         """
-        self.df_uwb['tx1'] *= ts_to_ns
-        self.df_uwb['rx1'] *= ts_to_ns
-        self.df_uwb['tx2'] *= ts_to_ns
-        self.df_uwb['rx2'] *= ts_to_ns
+        self.df_uwb['tx1'] *= self.ts_to_ns
+        self.df_uwb['rx1'] *= self.ts_to_ns
+        self.df_uwb['tx2'] *= self.ts_to_ns
+        self.df_uwb['rx2'] *= self.ts_to_ns
         
         if self.ds_twr:
-            self.df_uwb['tx3'] *= ts_to_ns
-            self.df_uwb['rx3'] *= ts_to_ns
+            self.df_uwb['tx3'] *= self.ts_to_ns
+            self.df_uwb['rx3'] *= self.ts_to_ns
             
     def drop_target_meas(self) -> None:
-        """_summary_
+        """If range measurements are stored at both initiating and targetted tag, 
+        this function removes measurements at the targetted tag to avoid duplicates.
         """
-        bool1 = (self.df_uwb['from_id'] != self.tag_ids[0])
-        bool2 = (self.df_uwb['from_id'] != self.tag_ids[1])
+        # Check if the initiating tag is not on this machine
+        bool = (self.df_uwb['from_id'] not in self.tag_ids)
         
-        self.df_uwb.drop(self.df_uwb[bool1 & bool2].index, inplace=True)
+        # Drop all rows that do satisfy the above condition
+        self.df_uwb.drop(self.df_uwb[bool].index, inplace=True)
         self.df_uwb.reset_index(inplace=True, drop=True)
         
     def merge_pose_data(self) -> None:
-        """_summary_
+        """Merge pose data from individual columns representing each dimension to
+        two columns of lists, one for position and one for quaternion attitude.
         """
+        # Merge position data.
         self.df_pose['r_iw_a'] = list(np.array((
-                                                self.df_pose['pose.position.x'],
-                                                self.df_pose['pose.position.y'],
-                                                self.df_pose['pose.position.z'],
-                                              )).T)
+            self.df_pose['pose.position.x'],
+            self.df_pose['pose.position.y'],
+            self.df_pose['pose.position.z'],
+        )).T)
+
+        # Merge quaternion attitude data.
         self.df_pose['q_ai'] = list(np.array((
-                                                self.df_pose['pose.orientation.x'],
-                                                self.df_pose['pose.orientation.y'],
-                                                self.df_pose['pose.orientation.z'],
-                                                self.df_pose['pose.orientation.w'],
-                                            )).T)
+            self.df_pose['pose.orientation.x'],
+            self.df_pose['pose.orientation.y'],
+            self.df_pose['pose.orientation.z'],
+            self.df_pose['pose.orientation.w'],
+        )).T)
         
-        self.df_pose.drop(columns=[
-                                    'pose.position.x',
-                                    'pose.position.y',
-                                    'pose.position.z',
-                                    'pose.orientation.x',
-                                    'pose.orientation.y',
-                                    'pose.orientation.z',
-                                    'pose.orientation.w',
-                                  ],
-                          inplace=True,
-                         )
+        # Drop the individual columns.
+        self.df_pose.drop(
+            columns=[
+                'pose.position.x',
+                'pose.position.y',
+                'pose.position.z',
+                'pose.orientation.x',
+                'pose.orientation.y',
+                'pose.orientation.z',
+                'pose.orientation.w',
+            ],
+            inplace=True,
+        )
         
 class RosMachine(Machine):
-    """_summary_
+    """A class to handle ROS machines recording pose and UWB data in rosbags.
+
+    All attributes from the Machine class are inherited.
 
     Attributes
     ----------
-    object : _type_
-        _description_
+    df_pose: pd.DataFrame
+        A dataframe for the ground-truth measurements with the following columns.
+        'time': float
+            ROS timestamp of the measurement, in seconds.
+        r_iw_a: list
+            The position of the machine relative to the motion-capture system's reference point.
+        q_ai: list
+            Quaternion parametrization of the robot's attitude, using the [x,y,z,w] convention.
+    df_uwb: pd.DataFrame
+        A dataframe for the UWB measurements with the following columns.
+        'time': float
+            ROS timestamp of the measurement, in seconds.
+        'range': float
+            The recorded range measurement.
+        'from_id': int
+            The ID of the initiating tag. 
+        'to_id': int
+            The ID of the target tag.
+        'tx1': float
+            The transmission timestamp of the first signal.
+        'rx1': float
+            The reception timestamp of the first signal.
+        'tx2': float
+            The transmission timestamp of the second signal.
+        'rx2': float
+            The reception timestamp of the second signal.
+        'tx3': float
+            The transmission timestamp of the third signal. 
+            Only exists if self.ds_twr == True.
+        'rx3': float
+            The reception timestamp of the third signal. 
+            Only exists if self.ds_twr == True. 
+        'fpp1': float
+            The received first-path power of the first signal.
+            Only exists if self.fpp_exists == True.
+        'fpp2': float
+            The received first-path power of the second signal.
+            Only exists if self.fpp_exists == True.
+        'rxp1': float
+            The receieved average power of the first signal. 
+            Only exists if self.rxp_exists == True.
+        'rxp2': float
+            The received average power of the second signal.
+            Only exists if self.rxp_exists == True.
+        'std1': int
+            The leading-edge detection algorithm's uncertainty on the first signal.
+            Only exists if self.std_exists == True.
+        'std2': int
+            The leading-edge detection algorithm's uncertainty on the second signal.
+            Only exists if self.std_exists == True.
 
     Examples
     --------
+    # *** Example reading a single machine ***
+    config_file = 'config/ifo_3_drones_rosbag.config'
+
+    parser = ConfigParser(interpolation=ExtendedInterpolation())
+    parser.read(config_file)
+
+    machine_num = 0
+    machine = RosMachine(parser, i)
+
+    # *** Example reading multiple machines ***
+    config_file = 'config/ifo_3_drones_rosbag.config'
+
+    parser = ConfigParser(interpolation=ExtendedInterpolation())
+    parser.read(config_file)
+
+    machines = {}
+    for i,machine in enumerate(parser['MACHINES']):
+        machine_id = parser['MACHINES'][machine]
+        machines[machine_id] = RosMachine(parser, i)
     """
     def __init__(
         self,
         configs,
         id,
-        ts_to_ns = 1,
         meas_at_target = False,
     ) -> None:
         """_summary_
 
         Parameters
         ----------
-        configs : _type_
-            _description_
-        id : _type_
-            _description_
-        ts_to_ns : int, optional
-            _description_, by default 1
+        configs : ConfigParser
+            A ConfigParser object where the config file is parsed.
+        id : int
+            The ID of the machine, as referenced in the config file.
         meas_at_target : bool, optional
-            _description_, by default False
+            Keep range measurements when initiator is not on this machine,
+            by default False
         """
         super().__init__(configs, id)
 
+        # Read the ground-truth and UWB data
         self.df_pose, self.df_uwb = self._read_data()
         
+        # pre-process data
         self.merge_pose_data()
-        self.convert_uwb_timestamps(ts_to_ns)
+        self.convert_uwb_timestamps()
         self._process_ros_timestamps()
         
+        # Drop duplicate measurements. 
         if not meas_at_target:
             self.drop_target_meas()
             
     def _read_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        """_summary_
+        """Read rosbags and extract pose and UWB information.
 
         Returns
         -------
-        _type_
-            _description_
+        df_pose: pd.DataFrame
+            A dataframe for the ground-truth measurements.
+        df_uwb: pd.DataFrame
+            A dataframe for the UWB measurements.
         """
         # Pose data 
         bag = bagreader(self.pose_path)
         pose_data = bag.message_by_topic(self.pose_topic)
         df_pose = pd.read_csv(pose_data)
         
+        # UWB data
         if self.uwb_path != self.pose_path:
             bag = bagreader(self.uwb_path)
         uwb_data = bag.message_by_topic(self.uwb_topic)
@@ -168,34 +286,34 @@ class RosMachine(Machine):
         return df_pose, df_uwb
         
     def _process_ros_timestamps(self) -> None:
-        """_summary_
+        """Process ROS timestamps.
         """
         self._merge_timestamp_headers(self.df_pose)
         self._merge_timestamp_headers(self.df_uwb)
         
     @staticmethod
     def _merge_timestamp_headers(df) -> None:
-        """_summary_
+        """Merges the ROS timestamp headers into one column that is both unwrapped
+        and with nanosecond accuracy.
+
+        The new column has the header "time, which merges and replaces the headers 
+        "header.stamp.secs" and "header.stamp.nsecs".
 
         Parameters
         ----------
-        df : _type_
-            _description_
+        df : pd.dataframe
+            Any Pandas dataframe with the following two columns.
+            "header.stamp.secs": int
+                The ROS timestamps, to the order of seconds.
+            "header.stamp.secs": int
+                The ROS timestamps, the nanoseconds portion.
         """
         df.drop(columns=["Time"],inplace=True)
         df['time'] = df["header.stamp.secs"] + df["header.stamp.nsecs"]/1e9
         df.drop(columns=["header.stamp.secs", "header.stamp.nsecs"],inplace=True)
 
 class CsvMachine(Machine):
-    """_summary_
-
-    Attributes
-    ----------
-    object : _type_
-        _description_
-
-    Examples
-    --------
+    """TODO: CsvMachine
     """
     def __init__(
         self,
@@ -204,23 +322,11 @@ class CsvMachine(Machine):
         ts_to_ns = 1,
         meas_at_target=False,
     ) -> None:
-        """_summary_
-
-        Parameters
-        ----------
-        configs : _type_
-            _description_
-        id : _type_
-            _description_
-        ts_to_ns : int, optional
-            _description_, by default 1
-        meas_at_target : bool, optional
-            _description_, by default False
-        """
         super().__init__(configs,
                          id,
                          ts_to_ns,
-                         meas_at_target)
+                         meas_at_target,
+                         is_ros = False)
 
         self.df_pose, self.df_uwb = self._read_data()
         
@@ -231,12 +337,4 @@ class CsvMachine(Machine):
             self.drop_target_meas()
         
     def _read_data(self) -> None:
-        """_summary_
-
-        Raises
-        ------
-        Exception
-            _description_
-        """
-        # TODO: implement reading data from csv files. 
         raise Exception("To be implemented.")
--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -9,14 +9,10 @@ config_file = 'config/ifo_3_drones_rosbag.config'
 parser = ConfigParser(interpolation=ExtendedInterpolation())
 parser.read(config_file)
 
-ts_to_ns = 1e9 * (1.0 / 499.2e6 / 128.0) # DW time unit to nanoseconds
-
 machines = {}
 for i,machine in enumerate(parser['MACHINES']):
     machine_id = parser['MACHINES'][machine]
-    machines[machine_id] = RosMachine(parser,
-                                      i,
-                                      ts_to_ns = ts_to_ns)
+    machines[machine_id] = RosMachine(parser, i)
 # %%
 data = PostProcess(machines)
 save(data, filename="data.pickle")
