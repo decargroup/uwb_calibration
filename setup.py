dbc8341d3510958cd1ca35e9f2e28c9071f96d0c
Mohammed A. Shalaby <mohammed.shalaby@mail.mcgill.ca>
2023-06-12 13:42:02 -0400

    minor bug

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'bagpy',
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
